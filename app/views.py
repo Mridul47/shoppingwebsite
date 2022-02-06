ff5e2bcfb5e38368662544007aeafbc8ea4aa977
Koirala <mrkoirala58@gmail.com>
2022-02-06 12:24:37 +0545

    made add to cart button dynamic

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlaced
 from.forms import CustomerRegistrationForm,CustomerProfileForm
@@ -21,7 +21,18 @@ class ProductDetailView(View):
         return render(request, 'app/productdetail.html', {'product':product})
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        return render(request, 'app/addtocart.html', {'carts':cart})
+
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
