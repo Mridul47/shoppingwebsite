6ccf55bf5eb9d84e3a0b1f900d3ce4fd5a08e569
Koirala <mrkoirala58@gmail.com>
2022-02-07 13:45:08 +0545

    order html backend done and fixed some bugs

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from.forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 from django.db.models import Q
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 
@@ -22,6 +24,7 @@ class ProductDetailView(View):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', {'product':product})
 
+@login_required
 def add_to_cart(request):
     user = request.user
     product_id = request.GET.get('prod_id')
@@ -29,6 +32,7 @@ def add_to_cart(request):
     Cart(user=user,product=product).save()
     return redirect('/cart')
 
+@login_required
 def show_cart(request):
     if request.user.is_authenticated:
         user = request.user
@@ -91,6 +95,7 @@ def minus_cart(request):
             }
         return JsonResponse(data)
 
+@login_required
 def remove_cart(request):
     if request.method == 'GET':
         prod_id = request.GET['prod_id']
@@ -115,13 +120,15 @@ def remove_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-
+@login_required
 def address(request):
     add=Customer.objects.filter(user=request.user)
     return render(request, 'app/address.html',{'add':add,'active':'btn-dark'})
 
+@login_required
 def orders(request):
- return render(request, 'app/orders.html')
+    op = OrderPlaced.objects.filter(user=request.user)
+    return render(request, 'app/orders.html',{'order_placed':op})
 
 
  
@@ -187,6 +194,7 @@ class CustomerRegistrationView(View):
             form.save()
         return render(request,'app/customerregistration.html',{'form':form})
 
+@login_required
 def checkout(request):
     user = request.user
     add = Customer.objects.filter(user=user)
@@ -202,6 +210,7 @@ def checkout(request):
         totalamount = amount + shipping_amount
     return render(request, 'app/checkout.html',{'add':add,'totalamount':totalamount,'cart_items':cart_items})
 
+@login_required
 def payment_done(request):
     user = request.user
     custid = request.GET.get('custid')
@@ -211,6 +220,8 @@ def payment_done(request):
         OrderPlaced(user=user, customer=customer, product=c.product, quantity=c.quantity).save()
         c.delete()
     return redirect("orders")
+
+@method_decorator(login_required,name='dispatch')
 
 class ProfileView(View):
     def get(self,request):
